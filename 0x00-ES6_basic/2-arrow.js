a250258a06a912c84ab71630e018de4be4a7e529
Jerethink <jerethink@gmail.com>
2023-06-27 17:32:34 +0000

    po

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -13,3 +13,9 @@ const getNeighborhoodsList = () => {
 
 export default getNeighborhoodsList;
 
+import getNeighborhoodsList from './2-arrow.js';
+
+const neighborhoodsList = getNeighborhoodsList();
+const res = neighborhoodsList.addNeighborhood('Noe Valley');
+console.log(res); // ['SOMA', 'Union Square', 'Noe Valley']
+
