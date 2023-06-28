bb84ca237867ff3245760f2ee5e86e4d373a948b
Jerethink <jerethink@gmail.com>
2023-06-28 00:45:06 +0000

    oi

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -1,21 +1,9 @@
-const getNeighborhoodsList = () => {
-  const sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+export default function getNeighborhoodsList() {
+  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
 
-  const addNeighborhood = (newNeighborhood) => {
-    sanFranciscoNeighborhoods.push(newNeighborhood);
-    return sanFranciscoNeighborhoods;
+  const self = this;
+  this.addNeighborhood = (newNeighborhood) => {
+    self.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return self.sanFranciscoNeighborhoods;
   };
-
-  return {
-    addNeighborhood
-  };
-};
-
-export default getNeighborhoodsList;
-
-import getNeighborhoodsList from './2-arrow.js';
-
-const neighborhoodsList = getNeighborhoodsList();
-const res = neighborhoodsList.addNeighborhood('Noe Valley');
-console.log(res); // ['SOMA', 'Union Square', 'Noe Valley']
-
+}
