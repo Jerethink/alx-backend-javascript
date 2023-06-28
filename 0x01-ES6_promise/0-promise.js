0791f5b8530e86b220d427fe17efb3e19d6b155a
Jerethink <jerethink@gmail.com>
2023-06-28 00:06:10 +0000

    push

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,15 +1,3 @@
-function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {
-    // Make an API request here
-    // You can use any library or method to make the request
-
-    // Simulating an API response after a delay of 1 second
-    setTimeout(() => {
-      const response = { data: 'API response data' };
-      // Resolve the promise with the response data
-      resolve(response);
-      // Or reject the promise with an error message
-      // reject('Error: Failed to fetch data from API');
-    }, 1000);
-  });
+export default function getResponseFromAPI() {
+  return new Promise(() => {});
 }
