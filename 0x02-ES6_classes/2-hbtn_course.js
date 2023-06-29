d096ee9ff5f79461d75f402db0d8395c483ddee2
Jerethink <jerethink@gmail.com>
2023-06-29 21:02:23 +0000

    push

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,30 +1,18 @@
-/**
- * Represents a Holberton Course.
- */
-export default class HolbertonCourse {
-  /**
-   * Creates a new @see {@link HolbertonCourse}.
-   *
-   * @param {String} name - The name of the course.
-   * @param {Number} length - How long the course is (in months).
-   * @param {String[]} students - The names of students in the course.
-   */
+class HolbertonCourse {
   constructor(name, length, students) {
+    this._name = '';
+    this._length = 0;
+    this._students = [];
+
     this.name = name;
     this.length = length;
     this.students = students;
   }
 
-  /**
-   * Gets the name of this course.
-   */
   get name() {
     return this._name;
   }
 
-  /**
-   * Sets the name of this course.
-   */
   set name(value) {
     if (typeof value !== 'string') {
       throw new TypeError('Name must be a string');
@@ -32,16 +20,10 @@ export default class HolbertonCourse {
     this._name = value;
   }
 
-  /**
-   * Gets the length of this course (in months).
-   */
   get length() {
     return this._length;
   }
 
-  /**
-   * Sets the length of this course (in months).
-   */
   set length(value) {
     if (typeof value !== 'number') {
       throw new TypeError('Length must be a number');
@@ -49,19 +31,13 @@ export default class HolbertonCourse {
     this._length = value;
   }
 
-  /**
-   * Gets the names of students in this course.
-   */
   get students() {
     return this._students;
   }
 
-  /**
-   * Sets the names of students in this course.
-   */
   set students(value) {
-    if (!(value instanceof Array)) {
-      throw new TypeError('Students must be an array of strings');
+    if (!Array.isArray(value)) {
+      throw new TypeError('Students must be an array');
     }
     if (!value.every((student) => typeof student === 'string')) {
       throw new TypeError('Students must be an array of strings');
@@ -69,3 +45,10 @@ export default class HolbertonCourse {
     this._students = value;
   }
 }
+
+// Example usage
+const course = new HolbertonCourse('Computer Science', 6, ['Alice', 'Bob', 'Charlie']);
+console.log(course.name);      // Output: Computer Science
+console.log(course.length);    // Output: 6
+console.log(course.students);  // Output: ['Alice', 'Bob', 'Charlie']
+
