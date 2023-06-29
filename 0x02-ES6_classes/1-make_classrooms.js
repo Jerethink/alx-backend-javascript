7ae6d8d7e911d1180362fbf84a100843d976fdaf
Jerethink <jerethink@gmail.com>
2023-06-29 15:44:16 +0000

    go

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,17 +1,9 @@
-import ClassRoom from './0-classroom.js';
+import ClassRoom from './0-classroom';
 
-function initializeRooms() {
-  const roomSizes = [19, 20, 34];
-  const classrooms = [];
-
-  for (let i = 0; i < roomSizes.length; i++) {
-    const size = roomSizes[i];
-    const classroom = new ClassRoom(size);
-    classrooms.push(classroom);
-  }
-
-  return classrooms;
+/**
+ * Creates an array of {@link ClassRoom}s with a specific size.
+ * @returns An array of {@link ClassRoom}s.
+ */
+export default function initializeRooms() {
+  return [19, 20, 34].map((size) => new ClassRoom(size));
 }
-
-const rooms = initializeRooms();
-console.log(rooms);
